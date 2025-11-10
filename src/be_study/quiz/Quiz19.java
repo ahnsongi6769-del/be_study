8960069cd6e3c68706b58fe76f3b297f4ae2d619
SONGI <ahnsongi6769@gmail.com>
2025-11-10 12:44:28 +0900

    JAVA 퀴즈 19 종합 (배열, 반복문)

---
+++ src/be_study/quiz/Quiz19.java
@@ -0,0 +1,119 @@
+package be_study.quiz;
+
+public class Quiz19 {
+
+	public static void main(String[] args) {
+		//2차원 배열과 인덱스 조정하기.
+		//2차원 배열을 활용하여 다음 결과가 나오도록 코드를 작성하시오.
+		//규칙을 찾아서 중첩된 반복문을 활용하여 값을 저장하고
+		//아래와 같이 4*4 형태로 출력하시오.
+
+//		1.
+//		1 2   3   4
+//		5 6   7   8
+//		9 10 11 12
+//		13 14 15 16
+//		int n = 1;
+//		int[][] arr = new int[4][4];
+//				for(int i=0; i<arr.length; i++) {
+//				for(int j=0; j<arr[i].length; j++) {
+//					arr[i][j] = n++;  
+//					
+//				}
+//			}
+//		
+//		//값 출력
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				System.out.printf("%3d",arr[i][j] );
+//
+//			}
+//			System.out.println();
+//		}
+		
+//		2.
+//		1 5  9  13
+//		2 6 10 14
+//		3 7 11 15
+//		4 8 12 16
+		
+		int num = 1;
+		int [][] arr2 = new int [4][4];
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				arr2[j][i] = num++;
+			}
+		}
+
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				System.out.printf("%3d",arr2[i][j] );
+			}System.out.println();
+			}
+//		3.
+//		16 15 14 13
+//		12 11 10 9
+//		8   7   6  5
+//		4   3   2  1
+
+		
+		
+//		int[][] arr3 = new int[4][4];
+//		int n1 = 16;
+//				for(int i=0; i<arr3.length; i++) {
+//				for(int j=0; j<arr3[i].length; j++) {
+//					arr3[i][j] = n1;  
+//					n1--;
+//				}
+//			}
+//		
+//		//값 출력
+//		for(int i=0; i<arr3.length; i++) {
+//			for(int j=0; j<arr3[i].length; j++) {
+//				System.out.printf("%3d", arr3[i][j]);
+//
+//			}
+//			System.out.println();
+//		}
+//		
+		System.out.println();
+		int n= 1;
+		int[][] arr3 = new int[4][4];
+		for(int i=0; i<4; i++) {
+			for (int j=0; j<4; j++) {
+				arr3[i][j]=n;
+				n++;
+			}
+		}
+		
+		for(int i=3; i>=0; i--) {
+			for (int j=3; j>=0; j--) {
+				System.out.printf("%3d", arr3[i][j]);
+			}System.out.println();
+		}
+
+			
+			//		4.
+//		16 12 8 4
+//		15 11 7 3
+//		14 10 6 2
+//		13 9  5  1
+	
+		int num1 = 1;
+		int [][] arr4 = new int [4][4];
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				arr2[i][j] = 16 - i - (4*j);
+			}
+		}
+
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				System.out.printf("%3d",arr2[i][j] );
+			}System.out.println();
+			}
+		
+
+	}}
+
+
