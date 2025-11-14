5021eac813e352092cba7bcd0c279a28afc146b7
SONGI <ahnsongi6769@gmail.com>
2025-11-14 10:10:51 +0900

    JAVA 1차 평가

---
+++ src/be_study/test/Level1.java
@@ -0,0 +1,43 @@
+package be_study.test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Level1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		
+		int user = 0;
+		int totalIning = 0;
+		
+		for(int i =1; i<=3; i++) {
+			int outCount = 0; 
+			int hitCount = 0;
+			while(outCount<3) {
+				System.out.print("게임시작 ( 1 ~ 10까지 정수를 입력하세요)");
+				user = scanner.nextInt();
+				int pitcher = (int)(Math.random()*10)+1; 
+				System.out.println("투수가 던진 공은? : "+ pitcher);
+				if(user == pitcher) {
+					hitCount++;
+					System.out.println("안타입니다.");
+				}else {
+					outCount++;
+					totalIning ++;
+					System.out.println("아웃입니다.");
+				}
+			}
+		}
+					
+	
+	
+
+		
+		
+	}
+
+}
