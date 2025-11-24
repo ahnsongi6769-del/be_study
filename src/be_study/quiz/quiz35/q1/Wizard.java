72506f5ac6ada7c55e2d117b64c5e5dca736c622
SONGI <ahnsongi6769@gmail.com>
2025-11-24 12:29:11 +0900

    퀴즈35 클래스 상속

---
+++ src/be_study/quiz/quiz35/q1/Wizard.java
@@ -0,0 +1,11 @@
+package be_study.quiz.quiz35.q1;
+
+public class Wizard extends Novice {
+
+	
+	int mp;
+	
+	public void fireball() {
+		System.out.printf("[%s]의 파이어볼@@\n", name);
+	}
+}
---
+++ src/be_study/quiz/quiz35/q2/Pet.java
@@ -0,0 +1,12 @@
+package be_study.quiz.quiz35.q2;
+
+public class Pet extends Animal{
+	int age;
+	int price;
+	
+	public void play() {
+		System.out.println("애완 동물이 장난을 칩니다.");
+	}
+	
+	
+}
