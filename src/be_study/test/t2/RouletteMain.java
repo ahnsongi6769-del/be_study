4e95af9a65fb2e6a838369cf3d2fb6b0df1d3bbb
SONGI <ahnsongi6769@gmail.com>
2025-11-27 10:14:02 +0900

    1,2차 테스트 자료

---
+++ src/be_study/test/t2/RouletteMain.java
@@ -0,0 +1,15 @@
+package be_study.test.t2;
+
+public class RouletteMain {
+
+	public static void main(String[] args) {
+		
+		Roulette rt = new Roulette("ben","잭팟명당");
+
+		
+		rt.startSales();
+
+		
+	}
+
+}
